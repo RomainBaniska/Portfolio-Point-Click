4e3e7d859709409773049437ba0892be8eb7860e
RomainBaniska <132657704+RomainBaniska@users.noreply.github.com>
2025-03-28 01:27:07 +0100

    ajouts de pages test pour l'écran d'entrée

---
+++ js/spoiler/spoiler.js
@@ -0,0 +1,31 @@
+export async function initializeApp() {
+
+    // Création d'une nouvelle application
+    const app = new PIXI.Application();
+
+    globalThis.__PIXI_APP__ = app;
+
+    // DEVTOOL
+    // window.__PIXI_DEVTOOLS__ = {
+    //     app
+    //   };
+
+    // Configuration de la largeur maximale adaptée à la taille de l'écran si plus petit
+    const maxWidth = 1440;
+    const width = Math.min(window.innerWidth, maxWidth);
+    const aspectRatio = 16 / 9;
+    const height = width / aspectRatio;
+
+    // Initialisation de l'application
+    await app.init({ 
+        width: width,
+        height: height,
+        backgroundColor: 0x1A1A50,
+    });
+
+    // Ajout du canvas de l'app au body
+    document.body.appendChild(app.canvas);
+
+return {app
+    }
+};
